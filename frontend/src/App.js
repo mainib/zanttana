8faa432637927626fd8b53bf9680855d75c127ab
mainib <mainib@mainibs-MacBook-Air.local>
2018-10-17 17:38:57 -0700

    simple front and back end todo app is done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  state= {
+    zanttas: []
+  }
+  async componentDidMount(){
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const zanttas = await res.json();
+      this.setState({
+        zanttas
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.zanttas.map(zantta => (
+          <div key={zantta.id}>
+            <h1>{zantta.title}</h1>  
+            <span>{zantta.description}</span>
+          </div>
+        ))}
       </div>
+        
     );
   }
 }
